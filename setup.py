937a5b4218e9ea4cb32cab01cc95127cb6201c0e
Ravi Kumar <2014pceevnecravi@poornima.org>
2023-06-20 17:26:27 +0530

    Model Trainer

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path:str)->List[str]:
        
 setup(
 name='mlproject',
-version='0.0.1',
+version='0.0.2',
 author='Ravi',
 author_email='ravi81073@gmail.com',
 packages=find_packages(),
