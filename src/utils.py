0d1abbf64b6153b281dd92798be0463042a9d72b
Ravi Kumar <2014pceevnecravi@poornima.org>
2023-06-21 19:13:21 +0530

    Hyperparameter Traning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 def save_object(file_path, obj):
@@ -20,16 +21,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         
         report= {}
         
         for i in range(len(list(models))):
-            
            model = list(models.values())[i]
+           para=param[list(models.keys())[i]]
+           
+           gs = GridSearchCV(model,para,cv=3)
+           gs.fit(X_train,y_train)
+           
+           model.set_params(**gs.best_params_)
+           model.fit(X_train,y_train)
             
-           model.fit(X_train, y_train)    # Train the model
+           # model.fit(X_train, y_train)    # Train the model
             
            y_train_pred = model.predict(X_train)
             
@@ -39,7 +46,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             
            test_model_score = r2_score(y_test, y_test_pred)
             
-           report[list(models.keys())[i]] = test_model_score
+           report[list(models.keys())[i]] = (test_model_score)
         
         return report
     
